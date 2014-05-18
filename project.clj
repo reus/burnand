8269e094e5384f7e49c86b4b500213b77e9bf7d1
Ruud Reus <ruudreus@gmail.com>
2014-05-18 21:24:09 +0200

    First commit

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject burnand "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.3"]
+                 [compojure "1.1.5"]
+                 [ring/ring-devel "1.1.8"]
+                 [ring/ring-core "1.1.8"]
+                 [cheshire "5.1.1"]
+                 [http-kit "2.1.12"]
+                 [com.novemberain/monger "1.5.0"]
+                 [clj-time "0.6.0"]
+                 [clj-pdf "1.11.17"]
+                 [enlive "1.1.4"]])
